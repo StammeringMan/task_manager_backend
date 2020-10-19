728840e6d53d733709e0f03ae1bb0b95c9f8abb4
Abhinav Alok <abhinav.alok02@gmail.com>
2020-10-19 14:04:46 +0530

    remove history files

--- .history/src/routes/index_20201019140148.js
+++
@@ -1,9 +0,0 @@
-import userRoute from "../api/users/userRoute.js";
-import taskRoute from "../api/tasks/taskRoute.js";
-import authRoute from "../api/auth/authRoute.js";
-
-export default function routes(app) {
-    app.use('/api/v1/tasks',  taskRoute);
-    app.use('/api/v1/users', userRoute);
-    app.use('/api/v1/auth', authRoute)
-}
